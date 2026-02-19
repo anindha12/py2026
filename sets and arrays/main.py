9a2c41fb29a139360112165b1ea783b4e95c03b5
anindha12 <anusky.anu@gmail.com>
2026-02-19 20:16:26 +0600

    cmmit

---
+++ sets and arrays/main.py
@@ -0,0 +1,8 @@
+dex = {"green", "blue"}
+dey = {"blue", "yellow"}
+print("Original set elements:")
+print(dex)
+print(dey)
+print("\nIntersection of two said sets:")
+dez = dex.union(dey)
+print(dez)
